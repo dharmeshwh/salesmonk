a62e269b774398c37c025d5bca53c3e88d1632fb
dharmeshwh <dharmeshaheer@gmail.com>
2024-01-09 23:29:57 +0530

    added dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import router from "./routes";
 import cors from "cors";
+import { configDotenv } from "dotenv";
+import path from "path";
 
 const app = express();
+
+configDotenv({ path: path.join(__dirname, "/../.env") });
 
 app.use(express.json());
 
